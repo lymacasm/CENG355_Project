432d48ff7f010b7e40fc71d862af8c10a098b64c
brownleepearson <brownleepearson@gmail.com>
2017-11-06 16:31:23 -0800

    Added a few lines

--- src/adc_dac.c
+++ src/adc_dac.c
@@ -4,13 +4,25 @@
  *  Created on: Oct 23, 2017
  *      Author: lymacasm
  */
+#include "stm32f0xx_adc.h"
+#include "adc_dac.h"
+
 
 extern void adc_dac_init()
 {
+	/* ------- ADC -------- */
 
+	GPIOC_MODER0 =
+
+	ADC->ADEN |=
+
+	//Initialize the ADC and DAC for use
+
+	//ADC will be on Pin PC0
+	//DAC will be on Pin PA4
 }
 
 extern int get_resistance_ohms()
 {
-
+	//This function will take the readings from the ADC and DAC and return the resistance
 }
