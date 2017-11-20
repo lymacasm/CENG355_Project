fd914529f893aa089f694caffbc557b9ee004f94
Lyden Smith <lymacasm@uvic.ca>
2017-11-20 14:28:19 -0800

    ADC is doing something promising

--- src/main.c
+++ src/main.c
@@ -51,10 +51,13 @@ main(int argc, char* argv[])
 	trace_printf("This is Part 2 of Introductory Lab...\n");
 	trace_printf("System clock: %u Hz\n", SystemCoreClock);
 
+	int i = 0;
 	while (1)
 	{
 		// Nothing is going on here...
 		// TODO: Create a timer interrupt to print frequency and period
+		trace_printf("ADC Conversion: %d\n", get_adc());
+		for(i = 0; i < 700; i++);
 	}
 
 	return 0;
